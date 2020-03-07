f89ef52c9649cf3e877a2d014d9f5914cd9cc9ca
jgysen <56094097+jgysen@users.noreply.github.com>
2020-03-07 17:18:47 +0100

    Add files via upload

---
+++ LCMconfig.ps1
@@ -0,0 +1,16 @@
+﻿[DSCLocalConfigurationManager()]
+configuration LCMConfig
+{
+    Node localhost
+    {
+        Settings
+        {
+            RefreshMode = 'Push'
+            RebootNodeIfNeeded = $True
+RefreshFrequencyMins =             30
+ConfigurationMode = 'ApplyAndAutoCorrect'
+
+        }
+    }
+}
+lcmconfig
